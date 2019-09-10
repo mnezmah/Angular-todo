3601abe0fd8e619877d582c6b9e43dddc5d745d9
mnezmah <mario.nezmah@gmail.com>
2019-09-10 10:21:10 +0200

    Set toggle method for todo-item checkbox

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-@Input() todo: Todo
+  @Input() todo: Todo
   constructor() { }
 
   ngOnInit() {
   }
 
+  //set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+  onToggle(todo) {
+    console.log('toggle')
+    todo.completed = !todo.completed
+  }
+  onDelete(todo) {
+    console.log('delete')
+  }
 }
